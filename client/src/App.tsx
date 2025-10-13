70480b60f4f9d86c0c21d0ff4eec91b9471f8595
TahaQadi <130837018+TahaQadi@users.noreply.github.com>
2025-10-13 16:56:13 +0000

    Restored to '73d92279b3b18e8fb629bec71bc5b7f76ef0c4c5'
    Replit-Restored-To: 73d92279b3b18e8fb629bec71bc5b7f76ef0c4c5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,6 @@ import AdminClientsPage from "@/pages/AdminClientsPage";
 import AdminLtaListPage from "@/pages/AdminLtaListPage";
 import AdminLtaDetailPage from "@/pages/AdminLtaDetailPage";
 import AdminPriceRequestsPage from '@/pages/AdminPriceRequestsPage';
-import AdminOffersPage from '@/pages/AdminOffersPage';
 import PriceRequestPage from '@/pages/PriceRequestPage';
 import ProductDetailPage from '@/pages/ProductDetailPage';
 import CatalogPage from '@/pages/CatalogPage';
@@ -95,7 +94,6 @@ function Router() {
       )}
       <AdminRoute path="/admin" component={AdminPage} />
       <AdminRoute path="/admin/products" component={AdminProductsPage} />
-      <AdminRoute path="/admin/offers" component={AdminOffersPage} />
       <AdminRoute path="/admin/vendors" component={AdminVendorsPage} />
       <AdminRoute path="/admin/clients" component={AdminClientsPage} />
       <AdminRoute path="/admin/price-requests" component={AdminPriceRequestsPage} />
